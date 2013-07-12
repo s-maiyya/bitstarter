c629dcbd1da2aa0ce551c1d5f3d74ee7b16d3cb7
s-maiyya <sindumaiya@gmail.com>
2013-07-12 07:06:27 +0000

    debugged web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var fs = require('fs');
-var filename = "index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(filename,'utf8');)
+  response.send(fs.readFileSync('index.html','utf8');)
 });
 
 var port = process.env.PORT || 5000;
