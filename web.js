8612ee6f79bf0cf31825630efea5565408735ac2
s-maiyya <sindumaiya@gmail.com>
2013-07-12 08:36:09 +0000

    this should work

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var text = fs.readFileSync('index.html','utf8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('WTF');
+  response.send(text.toString());
 });
 
 var port = process.env.PORT || 5000;
